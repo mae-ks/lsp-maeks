ac54ad48035a625873907f092f8d38054d9e4de5
maeky <mae.kyawsan@gmail.com>
2022-09-01 12:32:43 -0400

    This is my commit for assignment #1.

---
+++ lsp-maeks/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.howard.edu.lsp.assignment1;
+
+//Mae Kyaw San
+//@author mae-ks
+//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
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello LSP!!!!!");
+	}
+
+}
