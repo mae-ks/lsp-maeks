fa86889f30137b08e0144daacf73eff77e83cef4
maeky <mae.kyawsan@gmail.com>
2022-09-22 16:15:21 -0400

    Assignment 2

---
+++ lsp-maeks/src/org/howard/edu/lsp/assignment2/Combinations.java
@@ -0,0 +1,32 @@
+package org.howard.edu.lsp.assignment2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Combinations {
+	public void recurse(int[] arr, int target, List<Integer> curr, List<List<Integer>> all_sets, int start) {
+		int i, sum = 0;
+		for (i = 0; i < curr.size(); i++) {
+			int index = curr.get(i);
+			sum += arr[index];
+		}
+		if (sum == target) {
+			all_sets.add(curr);
+			return ;
+		}
+		
+		for (i = start; i < arr.length; i++) {
+			List<Integer> temp = new ArrayList<Integer>(curr);
+			temp.add(i);
+			recurse(arr, target, temp, all_sets, i+1);
+		}
+	}
+	
+	public void compute(int[] input, int target_sum) {
+		List<List<Integer>> sets = new ArrayList<>();
+		List<Integer> curr = new ArrayList<>();
+		
+		recurse(input, target_sum, curr, sets, 0);
+		System.out.println(sets);
+	}
+}
---
+++ lsp-maeks/src/org/howard/edu/lsp/assignment2/Driver.java
@@ -0,0 +1,11 @@
+package org.howard.edu.lsp.assignment2;
+
+public class Driver {
+	public static void main(String[] args) {
+		Combinations comb = new Combinations();
+		comb.compute(new int[] {5,5,15,10}, 15); // test case 1
+		comb.compute(new int[] {1,2,3,4,5,6}, 6); // test case 2
+		comb.compute(new int[] {}, 15); // test case 3
+		comb.compute(new int[] {1,2,3,4,5}, 16); // test case 4
+	}
+}
